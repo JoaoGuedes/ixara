a519302c4f2d86ce3dfcb6af51e2008b1a99b493
João Guedes <joaoguedes@boldint.com>
2016-06-20 17:22:35 +0100

    slider for desktop and mobile

--- slider/public/scripts/slider.js
+++ slider/public/scripts/slider.js
@@ -1,6 +1,60 @@
-"use strict";
+'use strict';
 
-[1, 2, 3].filter(function (item) {
-  return item > 1;
-});
+(function (window, undefined) {
+
+    var cursor = document.querySelector('.cursor'),
+        container = cursor.parentElement;
+
+    var dragging = false,
+        step = 100;
+
+    var getPosition = function getPosition(position) {
+        var result = position / (container.clientWidth - cursor.clientWidth);
+        return result;
+    };
+
+    var getCurrentPosition = function getCurrentPosition(mousePos, cursorWidth, containerWidth) {
+        if (mousePos - cursorWidth <= 0) {
+            return 0;
+        } else if (mousePos > containerWidth) {
+            return containerWidth - cursorWidth;
+        } else {
+            return mousePos - cursorWidth;
+        }
+    };
+
+    var moveHandler = function moveHandler(event) {
+        if (dragging) {
+            var pos = event.clientX,
+                currentPos = getCurrentPosition(pos, cursor.clientWidth, container.clientWidth);
+
+            console.log(getPosition(currentPos) * 255);
+
+            cursor.style.left = currentPos + 'px';
+        }
+    };
+
+    cursor.addEventListener('mousedown', function (event) {
+        dragging = true;
+        document.addEventListener('mousemove', moveHandler);
+    });
+
+    document.addEventListener('mouseup', function (event) {
+        dragging = false;
+        document.removeEventListener('mousemove', moveHandler);
+    });
+
+    cursor.addEventListener('touchstart', function (event) {
+        dragging = true;
+    });
+
+    cursor.addEventListener('touchend', function (event) {
+        dragging = false;
+    });
+
+    cursor.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+        moveHandler(event.changedTouches[0]);
+    });
+})(window, undefined);
 //# sourceMappingURL=slider.js.map
--- slider/src/slider.js
+++ slider/src/slider.js
@@ -1,1 +1,57 @@
-[1,2,3].filter((item) => item > 1);
+((window, undefined) => {
+
+    let cursor = document.querySelector('.cursor'),
+        container = cursor.parentElement;
+
+    let dragging = false, step = 100;
+
+    let getPosition = (position) => {
+        let result = position / (container.clientWidth - cursor.clientWidth);
+        return result;
+    };
+
+    let getCurrentPosition = (mousePos, cursorWidth, containerWidth) => {
+        if ((mousePos - cursorWidth) <= 0) {
+            return 0;
+        } else if (mousePos > containerWidth) {
+            return containerWidth - cursorWidth;
+        } else {
+            return mousePos - cursorWidth;
+        }
+    };
+
+    let moveHandler = (event) => {
+        if (dragging) {
+            let pos = event.clientX,
+                currentPos = getCurrentPosition(pos, cursor.clientWidth, container.clientWidth);
+
+            console.log(getPosition(currentPos) * 255);
+
+            cursor.style.left = `${currentPos}px`;
+        }
+    };
+
+    cursor.addEventListener('mousedown', (event) => {
+        dragging = true;
+        document.addEventListener('mousemove', moveHandler);
+    });
+
+    document.addEventListener('mouseup', (event) => {
+        dragging = false;
+        document.removeEventListener('mousemove', moveHandler);
+    });
+
+    cursor.addEventListener('touchstart', (event) => {
+        dragging = true;
+    });
+
+    cursor.addEventListener('touchend', (event) => {
+        dragging = false;
+    });
+
+    cursor.addEventListener('touchmove', (event) => {
+        event.preventDefault();
+        moveHandler(event.changedTouches[0]);
+    });
+
+})(window, undefined);
